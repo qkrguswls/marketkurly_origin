455118492404f0c6117c75bc471d263ba476069f
LAPTOP-UF8B9UM9\박현진 <qkrguswls309@naver.com>
2023-08-30 17:45:18 +0900

    23.08.30 복습

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,233 +1,93 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {ViewProductContext} from '';
+import React, {useState, useEffect, useRef} from 'react';
+import quickMenuHeaderImage from '';
+import iconArrow from '';
+import $ from 'jquery';
 
-export default function WrapComponent() {
+export default function QuickMenuComponent({최근본상품리스트}) {
 
-    const [viewProduct, setViewProduct] = useState({
-        viewProductKEY: '',
-        최근본상품: {},
-        isClick: false,
-        isTime: false,
-        최근본상품리스트: [],
-        imgPath: ''
-    });
-
-    const {viewProductKEY, 최근본상품, isClick, isTime, 최근본상품리스트} = viewProduct;
+    const [quickMenu, setQuickMenu] = useState(false);
+    const [list, setList] = useState(false);
+    const [refQuickMenu] = useRef();
 
     useEffect(()=>{
-        if (localStorage.getItem(viewProductKEY) !== null) {
-            setViewProduct({
-                ...viewProduct,
-                최근본상품리스트: JSON.parse(localStorage.getItem(viewProductKEY));
-            });
-        }
+        최근본상품리스트.length > 0 ? setList(true) : setList(false);
+    },[최근본상품리스트]);
+
+    useEffect(()=>{
+        window.addEventListener('scroll', function(){
+            let quickMenu = false;
+            if (window.scrollY >= 465) {
+                quickMenu = true;
+            } else {
+                quickMenu = false;
+            }
+            setQuickMenu(quickMenu);
+        });
+        $('#quickMenu .on').css({marginTop: $('#quickMenu').innerHeight()/2});
     },[]);
+
+    const viewProductBox = useRef();
+    const [cnt, setCnt] = useState(0);
+    const [isClick, setIsClick] = useState(false);
+
+    const onClickViewProduct=(e, value)=>{
+        e.preventDefault();
+        setIsClick(true);
+
+        if (value === 'up') {
+            setCnt(cnt >= 최근본상품리스트.length-3 ? 최근본상품리스트.length-3 : cnt+1);
+        } else {
+            setCnt(cnt <= 0 ? 0 : cnt-1);
+        }
+    }
 
     useEffect(()=>{
         if (isClick === true) {
-            setViewProduct({
-                ...viewProduct,
-                최근본상품: {
-                    ...최근본상품,
-                    이미지: viewProduct.imgPath,
-                    time: new Date().getTime()
-                },
-                isClick: false,
-                isTime: true
-            });
+            viewProductBox.current.style.top = `${-79 * cnt}px`;
+            setIsClick(false);
         }
-    },[최근본상품]);
-
-    useEffect(()=>{
-        let arr = [];
-
-        if (isTime === true) {
-            if (localStorage.getItem(viewProductKEY) !== null) {
-                arr = JSON.parse(localStorage.getItem(viewProductKEY));
-                arr = [
-                    최근본상품,
-                    ...arr
-                ];
-            } else {
-                arr = [최근본상품];
-            }
-            localStorage.setItem(viewProductKEY, JSON.stringify(arr));
-
-            setViewProduct({
-                ...viewProduct,
-                isTime: false,
-                최근본상품리스트: arr
-            });
-        }
-    },[최근본상품.time]);
-
-    const setViewProductFn=(value, imgPath)=>{
-        if (localStorage.getItem(viewProductKEY) !== null) {
-            let result = JSON.parse(localStorage.getItem(viewProductKEY));
-            const found = result.map((item)=>item.번호 === value.번호);
-
-            if (found.includes(true) === false) {
-                setViewProduct({
-                    ...viewProduct,
-                    최근본상품: value,
-                    isClick: true,
-                    imgPath: imgPath
-                });
-            }
-        } else {
-            setViewProduct({
-                ...viewProduct,
-                최근본상품: value,
-                isClick: true,
-                imgPath: imgPath
-            });
-        }
-    }
-
-    const [storageCookie] = useState({
-        key: '',
-        value: '',
-        expires: 1
-    });
-
-    const {key, value, expires} = storageCookie;
-    const [headerModal, setHeaderModal] = useState(true);
-
-    const setHeaderModalClose=()=>{
-        setHeaderModal(false);
-
-        let today = new Date();
-        today.setFullYear(today.getFullYeader()+expires);
-
-        const val = {
-            value: value,
-            expires: today.getTime()
-        }
-        localStorage.setItem(key, JSON.stringify(val));
-    }
-
-    useEffect(()=>{
-        if (localStorage.getItem(key) === null) {
-            return;
-        }
-        const topModal = JSON.parse(localStorage.getItem(key));
-
-        if (new Date() > new Date(topModal.expires)) {
-            setHeaderModal(true);
-        } else {
-            setHeaderModal(false);
-        }
-    },[]);
-
-    const [cookie] = useState({
-        cookieName: '',
-        cookieValue: '',
-        cookieExpires: 3
-    });
-
-    const {cookieName, cookieValue} = cookie;
-    const [introModal, setIntroModal] = useState(true);
-
-    const introModalClose=()=>{
-        setIntroModal(false);
-    }
-    const getCookie=()=>{
-        if (document.cookie === '') {
-            return;
-        }
-        let cookie = document.cookie.split(';');
-        let arr = [];
-
-        cookie.map((item, idx)=>{
-            arr[idx] = {
-                쿠키이름: item.split('=')[0].trim(),
-                쿠키값: item.split('=')[1].trim()
-            }
-        });
-        arr.map((item)=>{
-            if (item.쿠키이름 === cookieName && item.쿠키값 === cookieValue) {
-                setIntroModal(false);
-            }
-        })
-    }
-
-    useEffect(()=>{
-        getCookie();
-    },[introModal]);
-
-    const [confirm, setConfirm] = useState({
-        isConfirm: false,
-        msg: '',
-        type: 1,
-        timerStart: false
-    });
-
-    const {msg, type, isConfirm, timerStart} = confirm;
-
-    const confirmModalClose=()=>{
-        if (msg.includes('인증번호') === true) {
-            setConfirm({
-                isConfirm: false,
-                timerStart: true
-            });
-        } else {
-            setConfirm({
-                isConfirm: false
-            });
-        }
-        document.querySelector('html').style.overflowY = 'auto';
-    }
-    
-    const confirmModalOpen=(msg, type)=>{
-        setConfirm({
-            isConfirm: true,
-            msg: msg,
-            type: type
-        });
-        document.querySelector('html').style.overflow = 'hidden';
-    }
-
-    const [post, setPost] = useState({
-        isPostCode: false,
-        주소1: '',
-        주소2: '',
-        addressKey: ''
-    });
-
-    useEffect(()=>{
-        let result = '';
-        if (sessionStorage.getItem(post.addressKey) !== null) {
-            result = JSON.parse(sessionStorage.getItem(post.addressKey));
-            setPost({
-                ...post,
-                주소1: result.주소1,
-                주소2: result.주소2
-            });
-        }
-    },[]);
+    },[isClick]);
 
     return (
-        <div id="wrap">
-            <ViewProductContext.Provider value={{setViewProductFn, confirmModalClose, confirmModalOpen, msg, type, timerStart, post, setPost}}>
-                {headerModal && <HeaderModalComponent setHeaderModalClose={setHeaderModalClose}/>}
-
-                <BrowserRouter basename={process.env.PUBLIC_URL}>
-                    <Routes>
-                        <Route path='/' element={<HeaderComponent/>}>
-                            <Route index element={<MainComponent/>}/>
-                            <Route path='/main' element={<MainComponent/>}/>
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
-
-                <FooterComponent/>
-                {introModal && <IntroModalComponent introModalClose={introModalClose} cookie={cookie}/>}
-                <GoTopComponent/>
-                <QuickMenuComponent 최근본상품리스트={최근본상품리스트}/>
-                {isConfirm && <ConfirmComponent/>}
-                {post.isPostCode && <PostCodeComponent/>}
-            </ViewProductContext.Provider>
+        <div id="quickMenu" ref={refQuickMenu} className={quickMenu? 'on' : ''}>
+            <ul>
+                <li>
+                    <div><a href="!#"><img src={quickMenuHeaderImage} alt="" /></a></div>
+                </li>
+                <li><div></div></li>
+                <li>
+                    <div>
+                        <span><a href="!#">등급별 혜택</a></span>
+                        <span><a href="!#">레시피</a></span>
+                    </div>
+                </li>
+                <li><div></div></li>
+                <li>
+                    {
+                        list && (
+                            <div>
+                                <div className="view-top">
+                                    <a href="!#" onClick={(e)=>onClickViewProduct(e, 'down')}><img src={iconArrow}/></a>
+                                </div>
+                                <div className="view-box">
+                                    <ul ref={viewProductBox}>
+                                        {
+                                            최근본상품리스트.map((item, idx)=>{
+                                                return (
+                                                    <li key={item.번호}><a href="!#"><img src={item.이미지} alt="" /></a></li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                                <div className="view-bottom">
+                                    <a href="!#" onClick={(e)=>onClickViewProduct(e, 'up')}><img src={iconArrow} alt="" /></a>
+                                </div>
+                            </div>
+                        )
+                    }
+                </li>
+            </ul>
         </div>
     );
 };
