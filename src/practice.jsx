9d691adf7297b52844881c46b1f08ff09a21636b
LAPTOP-UF8B9UM9\박현진 <qkrguswls309@naver.com>
2023-09-12 16:47:16 +0900

    23.09.12 복습

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,25 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import goTopImage from '';
+import React from 'react';
+import Postcode from 'react-daum-postcode';
+import {ViewProductContext} from './components/context/ViewProductContext';
 
-export default function GoTopComponent() {
-
-    const [goTop, setGoTop] = useState(false);
-
-    useEffect(()=>{
-        window.addEventListner('scroll', function(){
-            let goTop = false;
-            if (window.scrollY > 1000) {
-                goTop = true;
-            } else {
-                goTop = false;
-            }
-            setGoTop(goTop);
-        });
-    },[]);
-
+export default function practice() {
     return (
-        <div id="goTop" className={goTop === true ? 'on' : 'off'}>
-            <a href="#wrap"><img src={goTopImage} alt="" /></a>
+        <div id="postCodeBox">
+            <div id="form">
+                <form>
+                    <ul>
+                        <li><h1>샛별배송 지역입니다.</h1></li>
+                        <li><h2>매일 새벽, 문 앞까지 신선함을 전해드려요.</h2></li>
+                        <li>
+                            <div>{post.주소1}</div>
+                            <button>재검색</button>
+                        </li>
+                        <li>
+                            <input type="text" id='add2' name='add2' value={post.주소2} onChange={onChangeAddress2}/>
+                        </li>
+                        <li>
+                            <p>저장된 배송지는 최대 7일 간 임시 저장 후 자동 삭제됩니다.</p>
+                        </li>
+                        li
+                    </ul>
+                </form>
+            </div>
         </div>
     );
 };
