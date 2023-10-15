16fa2a951a1309afaf4221b1313b128c746a2f6a
LAPTOP-UF8B9UM9\박현진 <qkrguswls309@naver.com>
2023-10-15 16:58:13 +0900

    23.10.15 복습2

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
+import {ViewProductContext} from '';
 
-export default function Section7Component() {
+export default function Section8SlideWrapSlide({상품}) {
+
+    const {setViewProductFn} = React.useContext(ViewProductContext);
+
+    const onClickViewProduct=(e, item, imgPath)=>{
+        e.preventDefault();
+        setViewProductFn(item, imgPath);
+    }
+
     return (
-        <section id="section7">
-            <div className="container">
-                <div className="gap">
-                    <div className="title hide"></div>
-                    <div className="content">
-                        <a href="!#"></a>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <ul className="slide-wrap">
+            {
+                상품.map((item, idx)=>{
+                    return (
+                        <li className="slide" key={item.번호}>
+                            <div className="slide-gap" onClick={(e)=>onClickViewProduct(e, item, `./img/intro/${item.이미지}`)}>
+                                <div className="img-box">
+                                    <img src={`./img/intro/${item.이미지}`} alt="" />
+                                    <span><img src="" alt="" /></span>
+                                </div>
+                                <div className="text-box">
+                                    <ul>
+                                        <li>{item.상품이름}</li>
+                                        <li>
+                                            {
+                                                item.할인율 > 0 ? (
+                                                    <>
+                                                        <strong>{Math.round(item.할인율*100)}%</strong>
+                                                        <span>{(item.정가*(1-item.할인율)).toLocaleString('ko-KR')}원</span>
+                                                        <em>{item.정가.toLocaleString('ko-KR')}원</em>
+                                                    </>
+                                                )
+                                                :
+                                                (
+                                                    `${item.정가.toLocaleString('ko-KR')}원`
+                                                )
+                                            }
+                                        </li>
+                                        <li>
+                                            <img src="" alt="" />
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    )
+                })
+            }
+        </ul>
     );
 };
